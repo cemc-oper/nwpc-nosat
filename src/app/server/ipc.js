5a0d907bfa365d98fdc2ccacc216025ffff410c0
perillaroc <perillaroc@gmail.com>
2017-11-16 21:39:46 +0800

    call system time line tool in ipc events.

--- src/app/server/ipc.js
+++ src/app/server/ipc.js
@@ -1,8 +1,10 @@
 const electron = require('electron');
 const ipc = electron.ipcMain;
+const { spawn } = require('child_process');
 
 const yaml = require('js-yaml');
 const fs   = require('fs');
+const path = require('path');
 
 const async = require("async");
 
@@ -30,18 +32,72 @@ ipc.on('analytics-tool.server.stop', function (event) {
 
 ipc.on('system-time-line.request.setup-env', function(event, config_file_path, repo_list){
   console.log('[ipc.js][system-time-line] request setup-env');
+  let system_running_time_analytics_config = null;
   try {
-    let system_time_line_config = yaml.safeLoad(fs.readFileSync(config_file_path, 'utf8'));
-    console.log(system_time_line_config);
+    config = yaml.safeLoad(fs.readFileSync(config_file_path, 'utf8'));
+    system_running_time_analytics_config = config['system_running_time_analytics'];
+    console.log(system_running_time_analytics_config);
   } catch (e) {
     console.error('[ipc.js][system-time-line] loading config file failed. Error is:');
     console.log(e);
     return;
   }
 
+  let system_time_line_config = system_running_time_analytics_config['system_time_line']['config'];
+  let system_time_line_project_base = system_running_time_analytics_config['system_time_line']['project']['base'];
+  let python_exe_path = system_running_time_analytics_config['system_time_line']['python']['exe_path'];
+
+  let system_time_line_config_path = path.join(
+    path.dirname(config_file_path),
+    system_time_line_config
+  );
+
+  let system_time_line_script_path = path.join(
+    system_time_line_project_base,
+    './nwpc_system_time_line/system_time_line_tool.py'
+  );
+
   async.eachSeries(repo_list, function(repo_object, callback){
     console.log("setup env for " + repo_object.owner + "/" + repo_object.repo);
+
+    // console.log(
+    //   python_exe_path, [
+    //     system_time_line_script_path,
+    //     'setup',
+    //     '--config=' + system_time_line_config_path,
+    //     '--owner=' + repo_object.owner,
+    //     '--repo=' + repo_object.repo
+    //   ]
+    // );
+
+    let env = process.env;
+    env.PYTHONPATH = ".";
+    const system_time_line_tool = spawn(python_exe_path, [
+      system_time_line_script_path,
+      'setup',
+      '--config=' + system_time_line_config_path,
+      '--owner=' + repo_object.owner,
+      '--repo=' + repo_object.repo
+    ], {
+      env: {
+        PYTHONPATH: system_time_line_project_base
+      }
+    });
+
+    system_time_line_tool.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    system_time_line_tool.stderr.on('data', (data) => {
+      console.log(`stderr: ${data}`);
+    });
+
+    system_time_line_tool.on('close', (code) => {
+      console.log(code);
+    });
+
     callback()
+
   }, function(err){
     if( err ) {
       console.error('[ipc.js][system-time-line]A repo is not set up.');
