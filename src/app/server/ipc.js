3f1480e0d366fd670d8212163bda4f606272e2a7
perillaroc <perillaroc@gmail.com>
2017-12-19 20:44:54 +0800

    delete unused code.

--- src/app/server/ipc.js
+++ src/app/server/ipc.js
@@ -111,30 +111,6 @@ function run_system_time_line_command(
 
 ipc.on('system-time-line.request.setup-env', function(event, config_file_path, repo_list){
   console.log('[ipc.js][system-time-line] request setup-env');
-  let system_running_time_analytics_config = null;
-  try {
-    config = yaml.safeLoad(fs.readFileSync(config_file_path, 'utf8'));
-    system_running_time_analytics_config = config['system_running_time_analytics'];
-    console.log(system_running_time_analytics_config);
-  } catch (e) {
-    console.error('[ipc.js][system-time-line] loading config file failed. Error is:');
-    console.log(e);
-    return;
-  }
-
-  let system_time_line_config = system_running_time_analytics_config['system_time_line']['config'];
-  let system_time_line_project_base = system_running_time_analytics_config['system_time_line']['project']['base'];
-  let python_exe_path = system_running_time_analytics_config['system_time_line']['python']['exe_path'];
-
-  let system_time_line_config_path = path.join(
-    path.dirname(config_file_path),
-    system_time_line_config
-  );
-
-  let system_time_line_script_path = path.join(
-    system_time_line_project_base,
-    './nwpc_system_time_line/system_time_line_tool.py'
-  );
 
   async.eachSeries(repo_list, function(repo_object, callback){
     console.log(`setup env for ${repo_object.owner}/${repo_object.repo}`);
